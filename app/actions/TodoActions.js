e99049fa2ee0948b3660cdea4bf0c87df6b113ae
karolgorecki <karol.gorecki@o2.pl>
2016-09-04 17:51:41 +0200

    Remove frontend part

--- app/actions/TodoActions.js
+++
@@ -1,8 +0,0 @@
-import * as actionType from '../constants/ActionTypes';
-
-export function addTodo(name) {
-    return {
-        type: actionType.ADD_TODO,
-        name
-    };
-}
--- app/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App'
-
-ReactDOM.render(<App />, document.getElementById('app-view'));
-
-// 3. BUILD THE UI!
